7810340ae7be970a3836b74d161d429fcfbdc2bb
alec.mcdaugale <mcdaale@linux-pc-8587.echostar.com>
2018-09-06 16:24:09 -0600

    Updated, still having problems with passing a string through a function, it was reccomended that I pass a reference to a pointer instead and return nothing.

--- c_testing_script/testing_c.c
+++ c_testing_script/testing_c.c
@@ -13,11 +13,20 @@
 int main()
 {
 	int max_num = 0;
+	int rand_num = 0;
+	char in_storage[100];
 
-	puts("Please enter a max number for a range.");
-	scanf(%d, &max_num);
+	max_num = get_num("Please enter a max number for a range.");
+	rand_num = get_rand_num(max_num);
 
-	puts("Guess a number between 0 and %d", max_num);
+
+	//printf("Guess a number between 0 and %d \n", max_num);
+	while(1)
+	{
+			get_num(get_message_w_num("Guess a number between 0 and", max_num));
+	}
+
+
 
 	return 0;
 }
@@ -29,4 +38,28 @@ int get_rand_num(int max)
 	random = rand() % max;
 	return random;
 }
+
+char *get_message_w_num(char string[], int max_num)
+{
+	char max_num_string[100];
+	sscanf((char)max_num, "%s", max_num_string);
+	strcat(string, max_num_string);
+
+	return string;
+}
+
+int get_num(char string[])
+{
+	int max_num = 0;
+	char in_storage[100];
+
+	puts(string);
+	fgets(in_storage, sizeof(in_storage), stdin);
+	sscanf(in_storage, "%d", &max_num);
+
+	return max_num;
+}
+
+
+
 
